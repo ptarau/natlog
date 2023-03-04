fe682ef816825589b4002fbcd7549f9b94c2bb58
Paul Tarau <ptarau@gmail.com>
2023-03-04 14:59:56 -0600

    finalized natgpt/deepchat

--- apps/natgpt/deepchat.py
+++ apps/natgpt/deepchat.py
@@ -5,7 +5,7 @@ import tiktoken
 from natlog import Natlog, natprogs, lconsult
 import json
 
-MAX_TOKENS = 1 << 12  # make shorter if needed
+MAX_TOKENS = 300 # 1 << 12  # make shorter if needed e.g. 300
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -14,32 +14,33 @@ class ChatMind:
     def __init__(self, max_toks=MAX_TOKENS, avatar=None):
         self.model = "gpt-3.5-turbo"
 
-        self.sys_prompt = [dict(role='system',
-                                content='You are a helpful assistant. Your answers are as concise as possible.')]
+        self.sys_prompt = dict(role='system',
+                               content='You are a helpful assistant. Your answers are as concise as possible.')
         self.max_toks = max_toks
         self.avatar = avatar
 
         if not self.resume():
-            self.short_mem = []
+            self.short_mem = dict()
             self.long_mem = dict()
-            self.answers = []
             self.toks = []
 
-    def add_content(self, role, content):
-
-        d = dict(role=role, content=content)
-        self.short_mem.append(d)
+    def to_message(self, quest):
+        mes = [self.sys_prompt]
+        for (q, a) in self.short_mem.items():
+            qd = dict(role='user', content=q)
+            ad = dict(role='assistant', content=a)
+            mes.extend([qd, ad])
+        mes.append(dict(role='user', content=quest))
+        return mes
 
     def get_state(self):
         return dict(long_mem=self.long_mem,
                     short_mem=self.short_mem,
-                    answers=self.answers,
                     toks=self.toks)
 
     def set_state(self, state):
         self.long_mem = state['long_mem']
         self.short_mem = state['short_mem']
-        self.answers = state['answers']
         self.toks = state['toks']
 
     def store_name(self):
@@ -50,7 +51,8 @@ class ChatMind:
         fname = self.store_name()
         if fname is not None and exists_file(fname):
             state = from_json(fname)
-            self.set_state(fname)
+            #print('!!!!',json.dumps(state,indent=4))
+            self.set_state(state)
             return True
         return False
 
@@ -60,35 +62,45 @@ class ChatMind:
         state = self.get_state()
         to_json(state, fname)
 
+    def already_answered(self,quest):
+        answer = self.short_mem.get(quest,None)
+        if answer is not None: return answer
+        answer = self.long_mem.get(quest,None)
+        return answer
+
     def __repr__(self):
         return json.dumps(self.get_state(), indent=2)
 
     def ask(self, quest):
-        if quest=='quit':
+        if quest == 'quit':
             self.persist()
             return 'bye'
+
+        answered = self.already_answered(quest)
+        if answered is not None:
+            return answered
 
         self.trim_context(quest)
 
         # print(len(self.answers), len(self.short_mem), len(self.toks))
 
-        assert 2*len(self.answers) == len(self.short_mem) == 2*len(self.toks)
+        assert len(self.short_mem) == len(self.toks)
 
-        self.add_content('user', quest)
+        mes = self.to_message(quest)
 
         r = openai.ChatCompletion.create(
             model=self.model,
-            messages=self.sys_prompt + self.short_mem
+            messages=mes
         )
         result = r['choices'][0]['message']['content']
-        result = result.split('\n')[-1]
-        self.answers.append(result)
+        answer = result.split('\n')[-1]
+
         t = r['usage']['total_tokens']
         self.toks.append(t)
 
-        self.add_content('assistant',result)
+        self.short_mem[quest] = answer
 
-        return result
+        return answer
 
     def trim_context(self, quest):
         if len(self.toks) == 0: return
@@ -100,11 +112,9 @@ class ChatMind:
         tok_estimate = total_toks + quest_toks + 2 * avg_toks  # conservative ...
 
         if tok_estimate > self.max_toks:
-            self.long_mem[self.short_mem[0]['content']] = self.answers[0]
-
+            k, v = dict_trim(self.short_mem)
+            self.long_mem[k] = v
             self.toks = self.toks[1:]
-            self.short_mem = self.short_mem[1:]
-            self.answers = self.answers[1:]
 
 
 # tools
@@ -146,6 +156,13 @@ def from_json(fname):
         return obj
 
 
+def dict_trim(d):
+    for k, v in d.items():
+        break
+    d.pop(k)
+    return k, v
+
+
 def run_natlog(natprog="deepchat.nat"):
     n = Natlog(file_name=natprog,
                with_lib=natprogs() + "lib.nat", callables=globals())
@@ -156,15 +173,17 @@ def run_natlog(natprog="deepchat.nat"):
 
 
 def test_deepchat():
-    cm = ChatMind()
+    cm = ChatMind(avatar='you')
 
     answer = cm.ask('What was the warmest temperature measured on Mars?')
     answer = cm.ask('The same question for planet Venus?')
     answer = cm.ask('And what about dwarf planet Ceres?')
     answer = cm.ask('Is it much colder on Pluto?')
+    print(f'ANSWER: {answer}\n')
     answer = cm.ask('quit')
+    print(answer,'\n')
 
-    print(cm)
+    print('STATE:\n',cm)
 
 
 if __name__ == "__main__":
