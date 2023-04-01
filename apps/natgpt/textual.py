72c6834c0f3b2134191b6257bede33901106ad08
Paul Tarau <ptarau@gmail.com>
2023-04-01 17:08:29 -0500

    refined processing of raw text seen as a Natlog textual db

---
+++ apps/natgpt/textual.py
@@ -0,0 +1,36 @@
+from natlog.natlog import Natlog,natprogs
+from natlog.textual_natlog import TextualNatlog
+import stanza
+
+def to_sents(text, lang='en'):
+    nlp = stanza.Pipeline(lang=lang, processors='tokenize')
+    doc = nlp(text)
+    sents = []
+    for sent in doc.sentences:
+        toks = []
+        for token in sent.tokens:
+            toks.append(token.text)
+        sent_text = " ".join(toks)
+        sents.append(sent_text)
+    return sents  # "\n".join(sents)
+
+def standardize_txt(fname,lang='en'):
+
+    with open(fname,'r') as f:
+        text=f.read()
+    sents=to_sents(text, lang=lang)
+    text="\n".join(sents)
+    with open(fname,'w') as g:
+        g.write(text)
+
+
+def start():
+  nname ="textual.nat"
+  dname = natprogs()+'../doc/prolog50.txt'
+  standardize_txt(dname)
+
+  n = TextualNatlog(file_name=nname, db_name=dname)
+  n.repl()
+
+if __name__=="__main__":
+    start()
--- natlog/tdb.py
+++ natlog/tdb.py
@@ -28,6 +28,29 @@ class Tdb(Db):
         super().__init__()
         self.index_source = const_of
 
+
+    def digest(self, text):
+        sents=text.split('\n')
+        for sent in sents:
+            ws=sent.split(' ')
+            if len(ws) < 2: continue
+            ws=[w for w in ws if w.isalpha()]
+            self.add_clause(ws)
+
+    def load_txt(self, fname):
+        """ assuming text tokenized, one sentence per line,
+         single white space separated
+        """
+        with open(fname) as f:
+            lines = f.read().split('\n')
+            for line in lines:
+                if len(line) < 2: continue
+                line = line.strip()
+                ws = line.split(' ')
+                ws = [w for w in ws if w.isalpha()]
+                self.add_clause(('txt', tuple(ws),))
+
+
     def unify_with_fact(self, h, trail):
         _txt, key, val = h
         ms = self.ground_match_of(key)
