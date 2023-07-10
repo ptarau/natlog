dc1915359406e45b988b0b9ffc13b63860bb1ba7
Paul Tarau <ptarau@gmail.com>
2023-07-09 22:28:34 -0500

    improved app, showing uploaded program text

--- natlog/app/natapp.py
+++ natlog/app/natapp.py
@@ -8,7 +8,7 @@ print('Running Natlog as a streamlit app!')
 
 st.set_page_config(layout="wide")
 
-st.title('Streamlit-based NatLog Client')
+st.title('Streamlit-based [NatLog](https://github.com/ptarau/natlog) Client')
 
 
 def ppp(*args):
@@ -22,24 +22,27 @@ suf = '.nat'
 
 def handle_uploaded(uploaded_file):
     if uploaded_file is not None:
-        fname = save_uploaded_file(uploaded_file)
+        fname,prog = save_uploaded_file(uploaded_file)
         suf0 = '.' + fname.split('.')[-1]
         if suf0 == suf:
-            return fname
+            return fname,prog
         else:
             ppp(f'Please chose a {suf} file!')
     else:
         ppp(f'Please upload your {suf} file!')
-
+    return None,""
 
 def save_uploaded_file(uploaded_file):
     name = uploaded_file.name
     fname = os.path.join(upload_dir, name)
-    if exists_file(fname): return fname
+    if exists_file(fname): return fname,file2string(fname)
     ensure_path(upload_dir)
+    bs = uploaded_file.getbuffer()
+    prog=str(bs)
+
     with open(fname, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return fname
+        f.write(bs)
+    return fname,prog
 
 
 def ensure_path(fname):
@@ -50,11 +53,17 @@ def ensure_path(fname):
 def exists_file(fname):
     return os.path.exists(fname)
 
+def file2string(fname):
+    with open(fname,'r') as f:
+        return f.read()
 
-fname = handle_uploaded(st.sidebar.file_uploader('Select a File', type=[suf]))
+fname,prog = handle_uploaded(st.sidebar.file_uploader('Select a File', type=[suf]))
 print(f'fname={fname}:')
 
+editor = st.text_area('Program',prog, height=320) # pixels
+
 with st.sidebar:
+
     question = st.text_area('Query?')
     query_it = st.button('Submit your question!')
 
@@ -63,10 +72,11 @@ def do_query():
     if fname is not None:
         lib = natprogs() + "lib.nat"
         if fname != lib:
-            with_lib=lib
+            with_lib = lib
         else:
-            with_lib=None
-        nat = Natlog(file_name=fname, with_lib=lib)
+            with_lib = None
+
+        nat = Natlog(file_name=fname, with_lib=with_lib)
 
     else:
         print('running with lib.nat')
